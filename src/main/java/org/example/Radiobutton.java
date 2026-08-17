d23d6608188c5fab56f3eadaafcd68e0821ffe17
ramprudhvi <desuramprudhvi@gmail.com>
2026-08-17 09:08:15 +0530

    all files put in one module

--- src/main/java/org/example/Radiobutton.java
+++ src/main/java/org/example/Radiobutton.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
--- src/main/java/org/example/draganddrop.java
+++ src/main/java/org/example/draganddrop.java
@@ -1,3 +1,5 @@
+package org.example;
+
 import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
